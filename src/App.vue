6399d15ac115e62b558d8aa8482604946e674c5c
Khun <lykhun@gmail.com>
2025-04-16 19:04:29 +0200

    interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { ref } from 'vue';
+const name = ref('world');
 </script>
 
 <template>
-    <p>Hello world !!</p>
+    <p>Hello {{ name }} !!</p>
 </template>
 
 <style scoped>
