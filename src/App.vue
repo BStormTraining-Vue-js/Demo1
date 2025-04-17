2205d949ca3b53186b47fe68968848bedc1f7026
Khun <lykhun@gmail.com>
2025-04-17 09:20:59 +0200

    reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup>
-const attributes = {
-    disabled: true,
-    type: 'reset',
-    class: 'btn-red',
-};
+import { onMounted, ref } from 'vue';
+// const count = ref(42);
+let countWithoutRef = 42;
+
+onMounted(() => {
+//   setInterval(() => {
+//     count.value++;
+//   }, 1000);
+
+  setInterval(() => {
+    countWithoutRef++;
+  }, 1000);
+});
 </script>
 
 <template>
-    <button v-bind="attributes">Click Me</button>
+    <!-- <p>Avec ref {{ count }}</p> -->
+    <p>Sans ref {{ countWithoutRef }}</p>
 </template>
 
 <style scoped>
-.btn-red {
-    background-color: red;
-    color: white;
-}
-.btn-red:disabled {
-    opacity: 0.5;
-}
 </style>
 
