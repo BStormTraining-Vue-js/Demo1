bac4ebe6f57430dc40ce34e81cfe32ad766b12a2
Khun <lykhun@gmail.com>
2025-04-18 01:41:39 +0200

    v-for list

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-const isLoggedIn = ref(false);
+import { onMounted, reactive } from 'vue';
+
+const movies = reactive([
+    { id: 1, title: 'E.T.'},
+    { id: 2, title: 'The Matrix'},
+    { id: 3, title: 'The Lord of the Rings'},
+]);
 
 onMounted(() => {
-    setInterval(() => {
-        isLoggedIn.value = true;
+    setTimeout(() => {
+        movies.push({ id: 4, title: 'Interstellar'});
+        movies.unshift({ id: 5, title: 'Inception'});
     }, 2000);
 });
 </script>
 
 <template>
-    <button v-show="!isLoggedIn">Login</button>
-    <button v-show="isLoggedIn">Logout</button>
+    <p>Sans tracking</p>
+    <ul>
+        <li v-for="(item, i) in movies">{{ item.title }} (index : {{ i }})</li>
+    </ul>
+    <hr>
+    <p>Avec tracking</p>
+    <ul>
+        <li v-for="(item, i) in movies" :key="item.id">{{ item.title }} (index : {{ i }})</li>
+    </ul>
 </template>
 
 <style scoped>
-
+    @keyframes appearing {
+        0% { opacity: 0; transform: translateX(-100px); }
+        100% { opacity: 1; transform: translateX(0); }    
+    }
+    li {
+        animation: appearing 1s ease-in-out forwards;
+    }
 </style>
 
