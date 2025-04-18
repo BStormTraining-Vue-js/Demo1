fad07e24b474c07c6d8ef5cdf5a3ff657c42e910
Khun <lykhun@gmail.com>
2025-04-18 15:07:25 +0200

    event handler

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <script setup>
-import { reactive } from 'vue';
-
-const movie = reactive({
-    id: 1,
-    title: 'Star Wars',
-    year: 1977,
-    director: 'George Lucas',
-    actors: ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher'],
-    duration: 121,
-});
-
+const clickHandler = (event) => {
+    console.log(event);
+    alert("Vous avez cliqué sur le bouton !");
+};
 </script>
 
 <template>
-    <dl>
-        <!-- la balise template ne sera pas rendue dans le DOM, elle sert juste pour la directive v-for -->
-        <template v-for="(item, key) in movie" :key>
-            <dt>{{ key }}</dt>
-            <dd>{{ item }}</dd>
-        </template>
-    </dl>
+    <button v-on:click="clickHandler">Click me!</button>
+    <!-- forme raccourcie -->
+    <button @click="clickHandler">Click me!</button>
 </template>
 
 <style scoped>
