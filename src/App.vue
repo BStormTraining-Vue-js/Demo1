e5942d8c2b42eee02639afaa6ee5c7c0fc8d5d1d
Khun <lykhun@gmail.com>
2025-04-20 09:45:31 +0200

    mouse button modifier

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-const log = console.log
+const leftClick = () => {
+    alert('Vous avez cliquez sur le bouton gauche !');
+};
+const rightClick = () => {
+    alert('Vous avez cliquez sur le bouton droit !');
+};
 </script>
 
 <template>
-    <!-- se déclenchera si la touche enter est relevée -->
-    <input @keyup.enter="log('enter')">
-    <!-- se déclenchera si les touches alt et enter sont baissées -->
-    <input @keydown.alt.enter="log('alt + enter')">
-    <!-- se déclenchera si la touche ctrl est baissée au moment du click  -->
-    <p @click.ctrl="log('click + ctrl')">Cliquer ici sur en appuyant sur enter en même temps</p>
+    <button @click.left="leftClick" @click.right.prevent="rightClick">Click me !</button>
 </template>
 
 <style scoped>
