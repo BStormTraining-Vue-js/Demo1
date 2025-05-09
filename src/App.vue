eb07b595b19f03e121b337b72d97cad913c68329
Khun <lykhun@gmail.com>
2025-05-09 17:11:15 +0200

    mouse modifier

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-const leftClick = () => {
-    alert('Vous avez cliquez sur le bouton gauche !');
+const middleClick = () => {
+    alert('Vous avez cliquez sur le bouton central !');
 };
 const rightClick = () => {
     alert('Vous avez cliquez sur le bouton droit !');
@@ -8,7 +8,7 @@ const rightClick = () => {
 </script>
 
 <template>
-    <button @click.left="leftClick" @click.right.prevent="rightClick">Click me !</button>
+    <button @click.middle="middleClick" @click.right.prevent="rightClick">Click me !</button>
 </template>
 
 <style scoped>
