196e29fcb6f812ab2c7bbc734f30459af69b2107
Khun <lykhun@gmail.com>
2025-05-18 15:26:50 +0200

    template ref

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import { reactive, watch } from 'vue';
-const obj = reactive({ nom: 'Khun', age: 43, adresse: { rue: 'Chemin des devs', ville: 'Bruxelles' } });
-watch(obj, () => {
-    console.log('Le nom ou l\'âge a changé');
+import { onMounted, useTemplateRef } from 'vue';
+
+const paragraphe = useTemplateRef('p');
+console.log(paragraphe.value); // null
+onMounted(() => {
+    console.log(paragraphe.value); // <p>Some text ...</p>
 })
-watch(() => obj.age, () => {
-    console.log('L\'âge a changé');
-})
-watch(() => obj.adresse, () => {
-    console.log('Ne se déclenche ici qu\'avec deep: true');
-}, /*{ deep: true }*/)
 </script>
  
 <template>
-    <div>
-        <input v-model="obj.nom">
-        <input v-model="obj.age">
-        <input v-model="obj.adresse.ville">
-    </div>
+    <p ref="p">Some text ...</p>
 </template>
 
 <style scoped>
